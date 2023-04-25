7c87b6230cf95f84994d153352a8d80dc2af3df7
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-04-25 21:45:23 +0500

    Setup getting the dogs

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-const queryFactsButton = document.querySelector("#query-facts");
+const queryImage = document.querySelector("#query-image");
+
+function setupBreedOptions(data) {
+  document.getElementById("breed").innerHTML = `
+    <select>
+      <option>Choose a dog breed</option>
+      ${Object.keys(data)
+        .map((breed) => {
+          return `<option>${breed}</option>`;
+        })
+        .join("")}
+    </select>
+    `;
+}
+
+async function getBreeds() {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await response.json();
+  setupBreedOptions(data.message);
+}
+
+function createSlideshow(images, breed) {
+  document.getElementById("slideshow").innerHTML = `
+      ${images.map((image) => {
+        return `<div class="card"><img src="${image}" alt="A ${breed}"></div>`;
+      })}
+      `;
+}
+
+async function getRandomImage() {
+  const breed = document.querySelector("#breed select").value;
+
+  if (breed === "Choose a dog breed") return;
+
+  const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+  const data = await response.json();
+
+  createSlideshow(data.message, breed);
+}
+
+queryImage.addEventListener("click", getRandomImage);
+getBreeds();
